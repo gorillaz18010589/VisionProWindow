7d4ff132f8a3fba91f3977823f2e5bc581023845
Hank <gorillaz1801058@gmail.com>
2025-01-29 20:49:53 +0800

    v2 feat 在window 新增SampleVIew

---
+++ VisionProWindow/SampleVIew.swift
@@ -0,0 +1,18 @@
+//
+//  SampleVIew.swift
+//  VisionProWindow
+//
+//  Created by 溫家豪 on 2025/1/29.
+//
+
+import SwiftUI
+
+struct SampleVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SampleVIew()
+}
